d22ce3b709b7f0663626d9b5f9a5582d1069a3eb
Arnold III <RARNOLD@houstonisd.org>
2018-12-10 16:36:27 -0600

    updated picture slide show. Will need to rethink layout for index.html

--- index.js
+++ index.js
@@ -7,24 +7,24 @@ images[1] = 'Espinosa-gets-award.jpg'
 images[2] = 'rams-become-coogs.jpg'
 images[3] = 'Graduation4.jpg'
 
-var pictures = []
+// var pictures = []
 
-pictures[0] = 'school-front.jpg'
-pictures[1] = 'Espinosa-gets-award.jpg'
-pictures[2] = 'rams-become-coogs.jpg'
-pictures[3] = 'Graduation4.jpg'
+// pictures[0] = 'school-front.jpg'
+// pictures[1] = 'Espinosa-gets-award.jpg'
+// pictures[2] = 'rams-become-coogs.jpg'
+// pictures[3] = 'Graduation4.jpg'
 
-// function changeImg () {
-//   document.slide.src = images[i]
+function changeImg () {
+  document.slide.src = images[i]
 
-//   if (i < images.length - 1) {
-//     i++
-//   } else {
-//     i = 0
-//   }
+  if (i < images.length - 1) {
+    i++
+  } else {
+    i = 0
+  }
 
-//   setTimeout('changeImg()', time)
-// }
+  setTimeout('changeImg()', time)
+}
 
 // function imgChange () {
 //   document.slide2.src = pictures[i]
@@ -37,13 +37,13 @@ pictures[3] = 'Graduation4.jpg'
 //   setTimeout('imgChange()', time)
 // }
 
-setInterval(function () {
-  // console.log(i)
-  document.slide.src = images[(i + 2) % images.length]
-  document.slide2.src = images[(i + 1) % images.length]
-  document.slide3.src = images[i % images.length ]
-  i++
-}, time)
+// setInterval(function () {
+//   // console.log(i)
+//   document.slide.src = images[(i + 2) % images.length]
+//   document.slide2.src = images[(i + 1) % images.length]
+//   document.slide3.src = images[i % images.length ]
+//   i++
+// }, time)
 
-// window.onload = changeImg
+window.onload = changeImg
 // window.onload = imgChange
